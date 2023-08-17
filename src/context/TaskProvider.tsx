86aeaa224c589f365a694e5ca93b19fb8d01f55e
Maxim <334890@gmail.com>
2023-08-17 16:17:21 +0600

    make TS happy

--- src/context/TaskProvider.tsx
+++ src/context/TaskProvider.tsx
@@ -1,9 +1,23 @@
 import { createContext, useContext, useReducer } from "react";
 
-const TaskStateContext = createContext(null);
-const TaskDispatchContext = createContext(null);
+type Task = {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+};
 
-export default function TaskProvider({ children }) {
+const initialState: Task[] = [];
+const defaultDispatch: React.Dispatch<Action> = () => initialState;
+const TaskStateContext = createContext(initialState);
+const TaskDispatchContext = createContext(defaultDispatch);
+
+type TaskProviderProps = {
+  children: React.ReactNode;
+};
+type TaskState = typeof initialState;
+type Action = { type: "ADD_TASK" };
+
+export default function TaskProvider({ children }: TaskProviderProps) {
   const [tasks, dispatch] = useReducer(taskReducer, initialState);
 
   return (
@@ -23,7 +37,7 @@ export function useTaskDispatch() {
   return useContext(TaskDispatchContext);
 }
 
-function taskReducer(state, action) {
+function taskReducer(state: TaskState, action: Action) {
   switch (action.type) {
     case "ADD_TASK":
       return state;
